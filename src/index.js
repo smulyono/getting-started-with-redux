6f44eef9413427bfc75c43b4225a281e9c0a595a
Sanny Mulyono <smulyono@me.com>
2017-07-03 00:46:15 -0700

    added react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import todoAppReducer from './reducer/todoapp';
 import {createStore} from 'redux';
 import TodoApp from './component/todoApp';
-import Provider from './component/Provider';
+// import Provider from './component/Provider';
+import { Provider } from 'react-redux';
 
 const render = () => {
     ReactDOM.render(
