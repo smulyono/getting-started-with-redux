05411d8c9689cccfe006634765b444579fb5748d
Sanny Mulyono <smulyono@me.com>
2017-07-04 09:13:17 -0700

    moving TodoLists to use connect, passing both state and dispatch of redux automatically

--- src/component/todoLists.js
+++ src/component/todoLists.js
@@ -1,120 +1,137 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const renderTodoBasedOnVisibility= (todos, visibility) => {
+    var outputTodo = [];
+    todos.map( (todo) => {
+        switch (visibility) {
+            case "completed" :
+                if (todo.completed) {
+                    outputTodo.push(todo);
+                } 
+            break;
+            case "progress" : 
+                if (!todo.completed) {
+                    outputTodo.push(todo);
+                }
+            break;
+            default : 
+                outputTodo.push(todo);
+            break;
+        }
+        return todo;
+    });
+    return outputTodo;
+}
 
 
 const TodoItem = ({
-    todo
-}, {
-    store
+    todos,
+    toggleTodoClick, 
+    removeTodoClick
 }) => (
-    <li style= {{
-            cursor : "pointer",
-        }}
-        > 
-        <span 
-        onClick={() => {
-            store.dispatch({
+    <ul>
+        {todos.map((todo) => {
+            return (
+                <li key={todo.id}
+                    style= {{
+                        cursor : "pointer",
+                    }}
+                    > 
+                    <span 
+                    onClick={() => {
+                        toggleTodoClick(todo)
+                    }}                            
+                    style={{
+                        textDecoration : todo.completed ? "line-through" : "none",
+                        fontWeight : todo.completed ? "normal" : "bold",
+                        color : todo.completed ? 'red' : 'black',
+                    }}>
+                    {todo.text} 
+                    </span>
+                    {todo.completed ? 
+                        <button 
+                            style={{
+                                borderStyle:"none",
+                                backgroundColor:"transparent",
+                                cursor: "pointer",
+                                textDecoration: "underline",
+                                fontWeight:"bold",
+                                color: "red",
+                                outline:"none"
+                            }}
+                            onClick={ () => {
+                                removeTodoClick(todo);
+                            }}
+                        >
+                            Remove
+                        </button> 
+                        : 
+                        ""  
+                    }
+                </li>  
+            )
+        })}
+    </ul>  
+)
+
+const mapStateIntoProps = (state) => {
+    return {
+        todos : renderTodoBasedOnVisibility(state.todos, state.visibility)
+    }
+};
+const mapDispatchIntoProps = (dispatch) => {
+    return {
+        toggleTodoClick : (todo) => {
+            dispatch({
                 ...todo,
                 type : "TOGGLE_TODO"
             })
-        }}                            
-        style={{
-            textDecoration : todo.completed ? "line-through" : "none",
-            fontWeight : todo.completed ? "normal" : "bold",
-            color : todo.completed ? 'red' : 'black',
-        }}>
-        {todo.text} 
-        </span>
-        {todo.completed ? 
-            <button 
-                style={{
-                    borderStyle:"none",
-                    backgroundColor:"transparent",
-                    cursor: "pointer",
-                    textDecoration: "underline",
-                    fontWeight:"bold",
-                    color: "red",
-                    outline:"none"
-                }}
-                onClick={ () => {
-                    store.dispatch( {
-                        id : todo.id,
-                        text : todo.text,
-                        type : "REMOVE_TODO"
-                    });
-                }}
-            >
-                Remove
-            </button> 
-            : 
-            ""  
+        },
+        removeTodoClick : (todo) => {
+            dispatch( {
+                id : todo.id,
+                text : todo.text,
+                type : "REMOVE_TODO"
+            });
         }
-    </li>    
-)
-TodoItem.contextTypes = {
-    store : PropTypes.object
-}
+    }
+};
 
-class TodoList extends React.Component {
-    constructor(props){
-        super(props);
-        this.renderTodoBasedOnVisibility = this.renderTodoBasedOnVisibility.bind(this);
-    }
+// Use react-redux connect to remove all scaffold to subscribe/unsubscribe context
+const TodoList = connect(mapStateIntoProps, mapDispatchIntoProps)(TodoItem);
 
-    componentDidMount() {
-        const { store } = this.context;
-        this.unsubscribe = store.subscribe(() => this.forceUpdate());
-    }
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
+// class TodoList extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.renderTodoBasedOnVisibility = this.renderTodoBasedOnVisibility.bind(this);
+//     }
 
-    renderTodoBasedOnVisibility(todos, visibility) {
-        var outputTodo = [];
-        todos.map( (todo) => {
-            switch (visibility) {
-                case "completed" :
-                    if (todo.completed) {
-                        outputTodo.push(todo);
-                    } 
-                break;
-                case "progress" : 
-                    if (!todo.completed) {
-                        outputTodo.push(todo);
-                    }
-                break;
-                default : 
-                    outputTodo.push(todo);
-                break;
-            }
-            return todo;
-        });
-        return outputTodo;
-    }
+//     componentDidMount() {
+//         const { store } = this.context;
+//         this.unsubscribe = store.subscribe(() => this.forceUpdate());
+//     }
+//     componentWillUnmount() {
+//         this.unsubscribe();
+//     }
+
     
-    render() {
-        const {store} = this.context;    
-        const {todos, visibility}  = store.getState();  
-        // determine todos to display
-        const drawTodos = this.renderTodoBasedOnVisibility(todos, visibility);
+//     render() {
+//         const {store} = this.context;    
+//         const {todos, visibility}  = store.getState();  
+//         // determine todos to display
+//         const drawTodos = this.renderTodoBasedOnVisibility(todos, visibility);
          
-        return (
-            <ul>
-            {drawTodos.map((todo) => {
-                return (
-                    <TodoItem 
-                        key={todo.id}
-                        todo={todo}
-                    />
-                )
-            })}
-            </ul>
-        )
-    }
-}
+//         return (
+//             <TodoItem 
+//                 todos = {drawT}
+//             />
+//         )
+//     }
+// }
 
-TodoList.contextTypes = {
-    store : PropTypes.object
-}
+// TodoList.contextTypes = {
+//     store : PropTypes.object
+// }
 
 export default TodoList;
