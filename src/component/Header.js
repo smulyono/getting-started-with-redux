0e772f8dcb4bae9cc14ca19d639e251765ed24fb
Sanny Mulyono <smulyono@me.com>
2017-07-04 09:13:42 -0700

    using connect in react-redux to pass dispatch into header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,37 +1,70 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 let todoCounter = 0;
 
-class Header extends React.Component {
-    render() {
-        const {store} = this.context;
-        const {label} = this.props;
-        return (
-            <div>
-                <input 
-                    type="text"
-                    ref ={ node => {
-                        this.inputText=node
-                    }}/>
-                <button onClick={() => {
-                    store.dispatch({
-                        text : this.inputText.value,
-                        id : todoCounter,
-                        type : "ADD_TODO"
-                    })
-                    todoCounter++;
-                    this.inputText.focus();
-                    this.inputText.value='';
-                }} >
-                    {label}
-                </button>            
-            </div>
-        );
-    }
+const HeaderItem = ({
+    label, 
+    dispatch
+}) => {
+    let inputText;
+    return (
+        <div>
+            <input 
+                type="text"
+                ref ={ node => {
+                    inputText=node
+                }}/>
+            <button onClick={() => {
+                dispatch({
+                    text : inputText.value,
+                    id : todoCounter,
+                    type : "ADD_TODO"
+                })
+                todoCounter++;
+                inputText.focus();
+                inputText.value='';
+            }} >
+                {label}
+            </button>            
+        </div>
+    )
 }
 
-Header.contextTypes = {
-    store : PropTypes.object
-}
+
+// with passing null on 2nd param, dispatch get passed into params,
+// instead passing explicit (dispatch) => { return {dispatch}}
+const Header = connect(null, null)(HeaderItem);
+
+// class Header extends React.Component {
+//     render() {
+//         const {store} = this.context;
+//         const {label} = this.props;
+//         return (
+//             <div>
+//                 <input 
+//                     type="text"
+//                     ref ={ node => {
+//                         this.inputText=node
+//                     }}/>
+//                 <button onClick={() => {
+//                     store.dispatch({
+//                         text : this.inputText.value,
+//                         id : todoCounter,
+//                         type : "ADD_TODO"
+//                     })
+//                     todoCounter++;
+//                     this.inputText.focus();
+//                     this.inputText.value='';
+//                 }} >
+//                     {label}
+//                 </button>            
+//             </div>
+//         );
+//     }
+// }
+
+// Header.contextTypes = {
+//     store : PropTypes.object
+// }
 export default Header;
